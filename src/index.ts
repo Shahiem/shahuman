a3d7f8bacaa1f0cd19c58a92f168e123964f0d1f
Shahiem <s.seymor@live.nl>
2020-01-29 00:20:42 -0300

    It's now possible to create a SVG - Fancy!

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import './sass/main.scss';
 import BodyInteraction from './app/BodyInteraction';
 import SkinColor from './app/SkinColor';
+import Head from './app/Head';
+
+// Set the body parts first to render
+let headParts = new Head();
 
 let bodyInteract = new BodyInteraction();
 let skinColorInteract = new SkinColor();
+
+// Stylesheets
+import './sass/main.scss';
